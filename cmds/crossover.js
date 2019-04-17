57a36b985d68baf6afa32165c1e7b1d767e8dbbb
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-17 16:30:13 +0700

    Create crossover.js

---
+++ cmds/crossover.js
@@ -0,0 +1,10 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    message.channel.send(`${message.author} crossed`);
+    let enbed = new Discord.RichEmbed() 
+};
+
+module.exports.help = {
+    name: "crossover"
+};
