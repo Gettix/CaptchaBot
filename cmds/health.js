938962781496fdb7f757ba34c1537cbe4a13ead2
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-17 17:16:10 +0700

    Update health.js

--- cmds/health.js
+++ cmds/health.js
@@ -1,6 +1,7 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
+    message.delete().catch();
     message.channel.send(`${message.author} health +30 hp`);
     let enbed = new Discord.RichEmbed() 
 };
