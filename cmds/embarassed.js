dfe84996f73afd048d87e9db29c26a635fe105e2
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-17 13:36:20 +0300

    Update embarassed.js

--- cmds/embarassed.js
+++ cmds/embarassed.js
@@ -1,1 +1,12 @@
-f
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    message.delete().catch();
+    message.channel.send(`${message.author} embarassed 😄`);
+    let enbed = new Discord.RichEmbed() 
+};
+
+module.exports.help = {
+    name: "embarassed"
+};
