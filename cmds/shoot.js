0f60fad529e7786001876bd28d82d10699cb0668
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-17 20:45:38 +0700

    Update shoot.js

--- cmds/shoot.js
+++ cmds/shoot.js
@@ -1,1 +1,23 @@
-D
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot,message,args) => {
+ 
+    let replies = ["-30 hp.", "-20 hp.", "Miss."];
+
+    let result = Math.floor((Math.random() * replies.length));
+    let question = args.slice(1).join(" ");
+
+    let ballembed = new Discord.RichEmbed()
+    .setAuthor(message.author.tag)
+    .addField("Цель", `${rUser}`)
+    .setColor('#FF9900')
+
+    .addField("Результат", replies[result]);
+
+    message.channel.send(ballembed);
+}
+
+module.exports.help = {
+    name: "shoot"
+};
+
