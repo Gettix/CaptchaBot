0bfd22fb1e6c919263315cca8428b7e64dd12fda
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-17 17:14:30 +0700

    Update attack.js

--- cmds/attack.js
+++ cmds/attack.js
@@ -2,6 +2,7 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    message.delete().catch();
     message.channel.send(`${message.author} attack ${rUser} -10 hp`);
     let enbed = new Discord.RichEmbed() 
 };
