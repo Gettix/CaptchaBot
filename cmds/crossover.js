b53c19f088bdca9ea3867236f6d13273e50298df
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-17 17:15:42 +0700

    Update crossover.js

--- cmds/crossover.js
+++ cmds/crossover.js
@@ -1,6 +1,7 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
+    message.delete().catch();
     message.channel.send(`${message.author} crossed ✝️`);
     let enbed = new Discord.RichEmbed() 
 };
