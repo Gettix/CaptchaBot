60fd4ff1e3a97ff14d17a67f06ca09f5ad7f659d
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-17 17:21:14 +0700

    Create hug.js

---
+++ cmds/hug.js
@@ -0,0 +1,12 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    message.delete().catch();
+    message.channel.send(`${message.author} hug ${rUser} 🤗`);
+    let enbed = new Discord.RichEmbed() 
+};
+
+module.exports.help = {
+    name: "hug"
+};
