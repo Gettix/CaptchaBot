f00f201e35edec9b9b68aab850545af33bad44c1
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-21 01:04:22 +0700

    Update rpc.js

--- cmds/rpc.js
+++ cmds/rpc.js
@@ -2,7 +2,7 @@ const Discord = module.require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    let replies = ["Rock💎", "paper📄", "scrisors✂️"];
+    let replies = ["Rock💎", "paper📄", "scissors✂️"];
 
     let result = Math.floor((Math.random() * replies.length));
     let question = args.slice(1).join(" ");
