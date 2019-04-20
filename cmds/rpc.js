9a3a6001f1d01cfed3800f71414ea396a29d1ab9
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-21 00:15:09 +0700

    Create rpc.js

---
+++ cmds/rpc.js
@@ -0,0 +1,22 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot,message,args) => {
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let replies = ["Rock💎", "paper📄", "scrisors✂️"];
+
+    let result = Math.floor((Math.random() * replies.length));
+    let question = args.slice(1).join(" ");
+
+    let ballembed = new Discord.RichEmbed()
+    .setAuthor(message.author.tag)
+    .addField("Цель", `${rUser}`)
+    .setColor('#FF9900')
+
+    .addField("Результат", replies[result]);
+
+    message.channel.send(ballembed);
+}
+
+module.exports.help = {
+    name: "shoot"
+};
