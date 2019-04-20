b5f71edd91b3791053aeccb8c7a9c57f32978695
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-21 01:59:33 +0700

    Update developer.js

--- cmds/developer.js
+++ cmds/developer.js
@@ -2,8 +2,8 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 let d = require('./botconfig.json');
 module.exports.run = async (bot,message,args) => {
-    let dev = d.dev
-    if(!message.author.dev) return message.channel.send("У Вас нет прав");
+
+    if(!message.user.username === d.dev) return message.channel.send("У Вас нет прав");
     message.channel.send('pong!');
     let enbed = new Discord.RichEmbed() 
 };
