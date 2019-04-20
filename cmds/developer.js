8580731e96c9897c1a05db59cd8a7cd0aba39cc8
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-21 01:38:59 +0700

    Update developer.js

--- cmds/developer.js
+++ cmds/developer.js
@@ -1,6 +1,8 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
+const d = require(./botconfig.json);
 module.exports.run = async (bot,message,args) => {
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("У Вас нет прав");
     message.channel.send('pong!');
     let enbed = new Discord.RichEmbed() 
 };
