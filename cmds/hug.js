c6772dd46fd60a801a251bcef14854f6806e965c
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-21 02:37:31 +0700

    Update hug.js

--- cmds/hug.js
+++ cmds/hug.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot,message,args) => {
     message.delete().catch();
     let embed = new Discord.RichEmbed();
     .addField(`${message.author} hug`, `${rUser} 🤗`);
-    .setColor("
+    .setColor("#FE0101")
     .setImage("https://cdn.discordapp.com/attachments/566235609882951681/569242723035512842/r9aU2xv.gif")
     message.channel.send(embed);
 };
