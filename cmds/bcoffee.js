4009b38c918057020ab404f14eb1a074f6cacd2e
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-21 21:34:27 +0700

    Create bcoffee.js

---
+++ cmds/bcoffee.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+let coins = require("../profile.json");
+
+module.exports.run = async (bot, message, args) => {
+  //!pay @isatisfied 59345
+
+  if(!coins[message.author.id]){
+    return message.reply("Вы не можете отправить монетки себе.")
+  }
+
+  let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+
+  
+
+
+  let sCoins = coins[message.author.id].coins;
+
+  if(sCoins < 50) return message.reply("Недостаточно монет.Для покупки нужно 50 монет.");
+
+  coins[message.author.id] = {
+    coins: sCoins - parseInt(50)
+  };
+
+
+
+
+
+  message.channel.send(`${message.author} buy coffee);
+
+  fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+    if(err) cosole.log(err)
+  });
+
+
+}
+
+module.exports.help = {
+  name: "buycoffee"
+}
