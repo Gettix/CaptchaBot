1ec8e45e20b9e4426de194921d2638d895d90cb7
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-21 20:37:17 +0700

    Update meme.js

--- cmds/meme.js
+++ cmds/meme.js
@@ -2,6 +2,7 @@ const Discord = module.require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
     let replies = ['https://i.redd.it/0ilh488xbudz.png',
+      'https://images.app.goo.gl/ed6EkptJRqF2dXt39',
       'https://cdn.discordapp.com/attachments/310611569794875404/353539349742092289/image.jpg',
       'http://weknowmemes.com/wp-content/uploads/2012/02/the-internet-is-a-series-of-tubes-and-theyre-full-of-cats.jpg',
       'http://assets8.popbuzz.com/2017/09/shooting-stars-meme-1488215847-list-handheld-0.png',
