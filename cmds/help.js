6bb42485f42808d186491bc6d4286a9d7815ee25
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-21 19:53:54 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot,message,args) => {
     .setColor('#4B0082')
     .addField("Модерация","kick(c!kick (пинг пользователя)) clear(c!clear (количество сообщений))  warn(c!warn (пинг пользователя))   unwarn(c!unwarn (пинг пользователя))  ban(c!ban (пинг пользователя))  say(c!say (ваше сообщение))  tempmute(c!tempmute (пинг пользователя) (сек./мин./час./дни))  report(c!report (пинг пользователя) (Причина или описание))")
     .addField("Экономика", "money (c!money)  pay (c!pay (имя получателя) (количество монеток))")
-    .addField("Развлечения", "8ball (c!8ball (вопрос))")
+    .addField("Развлечения", "8ball(c!8ball (вопрос))  meme(c!meme)")
     .addField("Информация","serverinfo,userinfo,help")
     .addField("Social", "embarassed, kiss (c!kiss (mmm#2222)), hug(c!hug (mmm#2222)), crossover")
     .addField("RolePlay", "health (c!health), attack (c!attack (primer#2222)), attack2 (c!attack2 (primer#2222)), kiss (c!kiss (primer#2222)), kill (c!kill (primer#2222))")
