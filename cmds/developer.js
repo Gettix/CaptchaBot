1deed597e7eccaf2c39bd5eb43b66775e5410682
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-22 13:39:13 +0700

    Update developer.js

--- cmds/developer.js
+++ cmds/developer.js
@@ -3,7 +3,7 @@ const conf = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
   //!coins
-  if(!message.author.id = conf.dev) return message.channel.send("You no developer");
+  if(!message.author.id == conf.dev) return message.channel.send("You no developer");
 
   message.channel.send('Love you, my creator')
 
