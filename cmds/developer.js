6b6b56a774b78d6b2599154e917562acf286a8b2
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-22 13:32:38 +0700

    Update developer.js

--- cmds/developer.js
+++ cmds/developer.js
@@ -1,1 +1,14 @@
 const Discord = require("discord.js");
+const conf = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+  //!coins
+  if(!message.author.id === conf.dev) return message.channel.send("You no developer");
+
+  message.channel.send("Love you, my creator)
+
+};
+
+module.exports.help = {
+  name: "infodev"
+}
