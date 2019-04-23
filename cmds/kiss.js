237fbdd6888f130ace35c1cd0a8931744e25b706
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-23 16:05:10 +0700

    Update kiss.js

--- cmds/kiss.js
+++ cmds/kiss.js
@@ -3,9 +3,13 @@ const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     message.delete().catch();
-    message.channel.send(`${message.author} kiss ${rUser} 😘`);
-    let kissI = ['https://cdn.discordapp.com/attachments/566235609882951681/570170470125535263/YQzo.gif', 'https://cdn.discordapp.com/attachments/566235609882951681/570170471442808843/FalseFarawayClam-size_restricted.gif', 'https://cdn.discordapp.com/attachments/566235609882951681/570170472197652490/anime-gif-kiss-11.gif']
-    let enbed = new Discord.RichEmbed() 
+   
+    let kissI = ['https://cdn.discordapp.com/attachments/566235609882951681/570170470125535263/YQzo.gif', 'https://cdn.discordapp.com/attachments/566235609882951681/570170471442808843/FalseFarawayClam-size_restricted.gif', 'https://cdn.discordapp.com/attachments/566235609882951681/570170472197652490/anime-gif-kiss-11.gif'];
+    let result = Math.floor((Math.random() * kissI.length));
+    let enbed = new Discord.RichEmbed()
+    .addField(message.author.username, `kiss ${rUser} 😚`)
+    .setImage(kissI)
+    message.channel.send(enbed)
 };
 
 module.exports.help = {
