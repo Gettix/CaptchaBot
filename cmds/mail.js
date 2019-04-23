148c09ded1a96609a47102167475d518221b1688
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-23 19:59:04 +0700

    Update mail.js

--- cmds/mail.js
+++ cmds/mail.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!rUser) return message.channel.send("Не удалось найти пользователя.");
+    
     let rreason = args.join(" ").slice(22);
     let rmsg = args.shift();
 
