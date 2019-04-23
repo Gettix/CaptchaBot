8dbf942f8961d0825b768fc503901690b26746e2
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-23 23:35:59 +0700

    Update shop.js

--- cmds/shop.js
+++ cmds/shop.js
@@ -5,6 +5,7 @@ module.exports.run = async (bot,message,args) => {
     .setDescription("Магазин")
     .setColor('#4B0082')
     .addField("💰 Валюта","1000:coffee: - 10 rub.", "1500:coffee: - 20 rub", "2300:coffee: - 30(🎁sale -9%)")
+    .addField("🎍 Способы оплаты", "🔸Qiwi - *8 913 001-59-97* (Указать что вы покупаете, а также ваш Никнейм и айди в Discord")
     .setFooter("CacaoBot 2019 | Все права защищены")
     .setThumbnail(message.guild.iconURL)
     .setImage('https://cdn.discordapp.com/attachments/566235609882951681/566235760311926784/rylgIUmPW.gif')
