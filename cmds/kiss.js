ad61ce7039203c5b0022d886dfbb52481a9d276e
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-23 16:19:05 +0700

    Update kiss.js

--- cmds/kiss.js
+++ cmds/kiss.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot,message,args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     message.delete().catch();
    
-    let kissI = ["https://cdn.discordapp.com/attachments/566235609882951681/570170470125535263/YQzo.gif", "https://cdn.discordapp.com/attachments/566235609882951681/570170471442808843/FalseFarawayClam-size_restricted.gif", "https://cdn.discordapp.com/attachments/566235609882951681/570170472197652490/anime-gif-kiss-11.gif'];
+    let kissI = ["https://cdn.discordapp.com/attachments/566235609882951681/570170470125535263/YQzo.gif", "https://cdn.discordapp.com/attachments/566235609882951681/570170471442808843/FalseFarawayClam-size_restricted.gif", "https://cdn.discordapp.com/attachments/566235609882951681/570170472197652490/anime-gif-kiss-11.gif"];
     let result = Math.floor((Math.random() * kissI.length));
     let enbed = new Discord.RichEmbed()
     .addField(message.author.username, `kiss ${rUser} 😚`)
