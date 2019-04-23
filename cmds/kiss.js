c9ea501ddfa59ac6138107998b0f955fd2098cb5
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-23 16:26:27 +0700

    Update kiss.js

--- cmds/kiss.js
+++ cmds/kiss.js
@@ -7,7 +7,8 @@ module.exports.run = async (bot,message,args) => {
     let kissI = ["https://cdn.discordapp.com/attachments/566235609882951681/570170470125535263/YQzo.gif", "https://cdn.discordapp.com/attachments/566235609882951681/570170471442808843/FalseFarawayClam-size_restricted.gif", "https://cdn.discordapp.com/attachments/566235609882951681/570170472197652490/anime-gif-kiss-11.gif"];
     let result = Math.floor((Math.random() * kissI.length));
     let enbed = new Discord.RichEmbed()
-    .addField("° message.author.username °", `kiss ${rUser} 😚`)
+    .setAuthor(message.author.username)
+    .addField(".•°kiss°•.", `😚 ${rUser} 😚`)
     .setImage(kissI[result])
     .setColor('#F8F8FF')
     message.channel.send(enbed)
