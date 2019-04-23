f9a6314cd0068401fd70aaf68a15138dcbe9d15c
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-24 00:32:29 +0700

    Update addcoin.js

--- cmds/addcoin.js
+++ cmds/addcoin.js
@@ -12,12 +12,7 @@ module.exports.run = async (bot, message, args) => {
   let ccoins = args.join(" ").slice(22);
   let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 
-  if(!coins[pUser.id]){
-    coins[pUser.id] = {
-      coins: 0
-    };
-  }
-
+  
   let pCoins = coins[pUser.id].coins;
   let sCoins = coins[message.author.id].coins;
 
