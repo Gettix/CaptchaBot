81364f3f1c873eaf7043dcb5a9e8cfda9252cb7b
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-24 22:59:15 +0700

    Update drinkcoffee.js

--- cmds/drinkcoffee.js
+++ cmds/drinkcoffee.js
@@ -1,4 +1,19 @@
 const Discord = require("discord.js");
 const profile = require("../profile.json");
 
-module.exports.run (message,args,err)
+module.exports.run = async (bot,message,args) => {
+  let uid = message.author.id;
+  let un = message.author.username;
+  bot.send = function (msg){
+    message.channel.send(msg);
+  }
+  if(!
+  
+  let u = profile[uid];
+  u.coins --;
+message.channel.send(`${un} Выпил кофе (-1)`)
+};
+
+module.exports.help = {
+  name:"drink"
+}
