da89a090289089210de93b0b9b9309b6a04038f5
Gettix <49359249+Gettix@users.noreply.github.com>
2019-05-09 16:03:52 +0300

    Update poll.js

--- cmds/poll.js
+++ cmds/poll.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-	let args = args.join(" ");
+	let argss = args.join(" ");
 	let poolembed = new Discord.RichEmbed()
-	.addField("Голосование", args)
+	.addField("Голосование", argss)
 	
 	bot.send(poolembed);
 	poolembed.react(":white_check_mark:");
