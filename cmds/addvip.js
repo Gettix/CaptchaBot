ff4e4642157523a912f7c77c18cb0eb51c50b963
Gettix <49359249+Gettix@users.noreply.github.com>
2019-09-28 18:46:10 +0300

    Update addvip.js

--- cmds/addvip.js
+++ cmds/addvip.js
@@ -1,6 +1,6 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
-const profile = require('./profile.json');
+let profile = require("../profile.json");
 
 module.exports.run = async (bot,message,args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
@@ -10,6 +10,8 @@ module.exports.run = async (bot,message,args) => {
     if(Owner.id !== devel.dev) return message.reply("Only the bot owner can use this command!")
     
     u.vip++;
+    fs.writeFile('./profile.json',JSON.stringify(profile),(err) =>{
+        if(err) console.log(err);
     
     message.channel.send('Успешно! Теперь этот пользователь имеет вип-доступ');
     };
