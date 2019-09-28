145d61584ec1c899d5b6e6c9bc80d4069ed28b09
Gettix <49359249+Gettix@users.noreply.github.com>
2019-09-28 18:27:34 +0300

    Update and rename addvip to addvip.js

---
+++ cmds/addvip.js
@@ -0,0 +1,13 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+let profile = require('./profile.json');
+
+module.exports.run = async (bot,message,args) => {
+    let uid = usr.id;
+    let u = profile[uid];
+    let usr = 
+    
+
+module.exports.help = {
+    name: "addvip"
+};
