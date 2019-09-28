09fcf6c0559b9a35c015776c758c0425102a5e36
Gettix <49359249+Gettix@users.noreply.github.com>
2019-09-28 18:36:41 +0300

    Update addvip.js

--- cmds/addvip.js
+++ cmds/addvip.js
@@ -5,8 +5,13 @@ let profile = require('./profile.json');
 module.exports.run = async (bot,message,args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     let uid = rUser.id;
+    let Owner = message.author;
     let u = profile[uid];
+    if(Owner.id !== devel.dev) return message.reply("Only the bot owner can use this command!")
+    
     u.vip++;
+    
+    let 
     
 
 module.exports.help = {
