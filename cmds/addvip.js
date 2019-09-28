041d20c3ebefc65dd760e95506733d0e7e2ceb76
Gettix <49359249+Gettix@users.noreply.github.com>
2019-09-28 18:33:06 +0300

    Update addvip.js

--- cmds/addvip.js
+++ cmds/addvip.js
@@ -3,9 +3,10 @@ const fs = require("fs");
 let profile = require('./profile.json');
 
 module.exports.run = async (bot,message,args) => {
-    let uid = usr.id;
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let uid = rUser.id;
     let u = profile[uid];
-    let usr = 
+    u.vip++;
     
 
 module.exports.help = {
