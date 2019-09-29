36bf07d9259559873efc527d2b1b9361790c2944
Gettix <49359249+Gettix@users.noreply.github.com>
2019-09-29 18:22:35 +0300

    Update hug.js

--- cmds/hug.js
+++ cmds/hug.js
@@ -1,6 +1,12 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
+let p = require("../profile.json");
+
 module.exports.run = async (bot,message,args) => {
+    let au = message.author.id
+    let Owner = p[au]
+    if(Owner.vip !== true) return message.reply("Только вип-пользователи могут использовать эту команду!")
+    
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     message.delete().catch();
     let embed = new Discord.RichEmbed()
