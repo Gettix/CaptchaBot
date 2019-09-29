800277e45da33071e22cf0b0ba1194bce3778c7a
Gettix <49359249+Gettix@users.noreply.github.com>
2019-09-29 18:21:25 +0300

    Update embarassed.js

--- cmds/embarassed.js
+++ cmds/embarassed.js
@@ -1,6 +1,12 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
+let p = require("../profile.json");
+
 module.exports.run = async (bot,message,args) => {
+    let au = message.author.id
+    let Owner = p[au]
+    if(Owner.vip !== true) return message.reply("Только вип-пользователи могут использовать эту команду!")
+    
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     message.delete().catch();
     message.channel.send(`${message.author} embarassed 😄`);
