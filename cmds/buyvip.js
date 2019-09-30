e1d3756899bada7e2f081954452df62173325aee
Gettix <49359249+Gettix@users.noreply.github.com>
2019-09-30 21:44:00 +0700

    Update buyvip.js

--- cmds/buyvip.js
+++ cmds/buyvip.js
@@ -1,1 +1,24 @@
-s
+const Discord = module.require("discord.js");
+const fs = require("fs");
+const devel = require('../botconfig.json');
+let profile = require("../profile.json");
+
+module.exports.run = async (bot,message,args) => {
+    let Owner = message.author;
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let uid = Owner.id;
+    let u = profile[uid];
+    if(u.coins < 3000) return message.reply("Не хватает средств!")
+    
+    u.vip = true;
+    u.coins - 3000;
+  
+
+    
+    message.channel.send('Успешно! Вы купили вип-доступ (До следующей перезагрузки бота)');
+};
+    
+
+module.exports.help = {
+    name: "addvip"
+};
