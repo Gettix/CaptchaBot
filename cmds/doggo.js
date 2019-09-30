df8b3029b1f299371c88fee95d201fb2734576c3
Gettix <49359249+Gettix@users.noreply.github.com>
2019-09-30 21:23:18 +0700

    Update doggo.js

--- cmds/doggo.js
+++ cmds/doggo.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js");
-//const superagent = require("superagent");
+const superagent = require("superagent");
 
 module.exports.run = async (bot,message,args) => {
   
-  //let {body} = await superagent.get(`https://random.dog/woof.json`);
+  let {body} = await superagent.get(`https://random.dog/woof.json`);
   
   let dogembed = new Discord.RichEmbed()
   .setColor("#ff9900")
   .setTitle("doggo")
-  //.setImage(body.url);
+  .setImage(body.url);
   
   message.channel.send(dogembed);
 }
