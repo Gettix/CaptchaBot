865de9fa9da27313a375c806d4d9a64fe53f8d1d
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-01 19:05:02 +0300

    Update (gamestart)word.js

--- cmds/(gamestart)word.js
+++ cmds/(gamestart)word.js
@@ -3,18 +3,17 @@ const fs = require("fs");
 const g = require("../game.json");
 module.exports.run = async (bot,message,args) => {
     if(g.starts !== true) return bot.send("Игра не началась. Начните игру (.whstart)");
-    let set = args.join(" ");
     let wordd = args.pop();
     
-    if(set === 'set') {
+    if ( args.length === 1 && args[ 0 ] === 'set' ) {
     if(g.word === 'none') return bot.send("Слово уже выбранно");
     g.word = wordd;
     bot.send(`Слово выбрано. Ведущий ${message.author}. Введите .word is <word> для отгадки слова.`);
     }
     
-    if(set === 'is') {
+    if ( args.length === 1 && args[ 0 ] === 'is' ) {
     if(wordd !== g.word) return bot.send(`${message.author}, Вы не угадали слово, попробуйте еще раз`);
-    if(wordd === g.word) {
+    if(wordd == g.word) {
     g.starts = false;
     bot.send(`${message.author} отгадал слово! Игра окончена:3`);
    };
