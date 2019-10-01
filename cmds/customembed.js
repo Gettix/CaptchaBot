3189fe9803c903aedc8e5b1abfdbbe5a2299de1b
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-01 18:24:23 +0700

    Create customembed.js

---
+++ cmds/customembed.js
@@ -0,0 +1,10 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    let title = args(0);
+    let enbed = new Discord.RichEmbed() 
+};
+
+module.exports.help = {
+    name: "cembed"
+};
