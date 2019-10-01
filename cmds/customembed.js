7cbc97df83b07e37df6b270c0d052f84286cff83
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-01 18:44:23 +0700

    Update customembed.js

--- cmds/customembed.js
+++ cmds/customembed.js
@@ -1,8 +1,14 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
-    let title = args(0);
+    let text = args(3);
+    let name = args.pop();
+    let title = args.join( " " );
     let enbed = new Discord.RichEmbed() 
+    .setTitle(title)
+    .addField(name, text)
+    
+    bot.send(enbed);
 };
 
 module.exports.help = {
