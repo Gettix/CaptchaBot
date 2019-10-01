18f71cb5e0cfbb96730c20eff70a13826983654c
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-01 17:45:54 +0300

    Create (gamestart)word.js

---
+++ cmds/(gamestart)word.js
@@ -0,0 +1,24 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+const g = require("../game.js");
+module.exports.run = async (bot,message,args) => {
+    if(g.start !== true) return bot.send("Игра не началась. Начните игру (.whstart)");
+    let set = args.join(" ");
+    let wordd = arps.pop("");
+    
+    if(set == "set") {
+    if(g.word == "none") return bot.send("Слово уже выбранно");
+    g.word == wordd;
+    bot.send(`Слово выбрано. Ведущий ${message.author}. Введите .word is <word> для отгадки слова.`);
+    }
+    
+    if(set == "is") {
+    if(wordd !== g.word) return bot.send(`${message.author}, Вы не угадали слово, попробуйте еще раз`);
+    if(wordd == g.word) {
+    g.starts = false;
+    bot.send(`${message.author} отгадал слово! Игра окончена:3`);
+};
+
+module.exports.help = {
+    name: "word"
+};
