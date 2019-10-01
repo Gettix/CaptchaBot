3fc1d332fea2f3f5b27e35cca4cbf84383a741f6
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-01 14:27:18 +0700

    Update poll.js

--- cmds/poll.js
+++ cmds/poll.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 	let argss = args.join(" ");
 	let poolembed = new Discord.RichEmbed()
+	.setTitle("Привет!")
 	.addField("Голосование", argss);
 	
 	bot.send(poolembed);
