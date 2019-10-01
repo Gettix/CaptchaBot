194c6f4e1033e74fde42916d7f92cff895961b9e
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-01 17:22:27 +0300

    Update gamestart.js

--- cmds/gamestart.js
+++ cmds/gamestart.js
@@ -1,1 +1,16 @@
-cc
+
+const Discord = module.require("discord.js");
+const fs = require("fs");
+const g = require('../game.json');
+
+module.exports.run = async (bot,message,args) => {
+    if(g.starts == true) return bot.send("Игра уже началась");
+    g.starts == true;
+    
+  bot.send("Игра началась!! Кто первый введет слово тот и будет ведущим! (.word set <word>)");
+  
+};
+
+module.exports.help = {
+    name: "whstart"
+};
