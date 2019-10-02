c49a985653228360e1dd1c12e3f03eec1e719b09
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-02 17:10:08 +0300

    Update Shopnameset.js

--- cmds/Shopnameset.js
+++ cmds/Shopnameset.js
@@ -2,6 +2,7 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 const g = require("../guildsshop");
 module.exports.run = async (bot,message,args) => {
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("У Вас нет прав");
     let name = args.join(" ");
     g.namerole = name;
     message.channel.send(`Имя роли сменено на ${name}`);
