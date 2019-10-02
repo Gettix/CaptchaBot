56e7f01fe52f7e0a5fbce850cf05030d4e4db633
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-02 04:22:21 +0300

    Update (gamestart)word.js

--- cmds/(gamestart)word.js
+++ cmds/(gamestart)word.js
@@ -15,6 +15,8 @@ module.exports.run = async (bot,message,args) => {
     
     if ( args.length === 1 && args[ 0 ] === 'is' ) {
     if(wordd !== g.word) return bot.send(`${message.author}, Вы не угадали слово, попробуйте еще раз`);
+    if(g.word == "none") return bot.send("Слово еще не выбранно. Выберите слово .word set);
+                                     
     if(wordd == g.word) {
     g.starts = false;
     g.word = "none";
