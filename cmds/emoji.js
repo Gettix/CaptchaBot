9ef4d6f3d51584f02ac9f28ae613f73b429c4117
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-02 18:25:21 +0300

    Update emoji.js

--- cmds/emoji.js
+++ cmds/emoji.js
@@ -1,8 +1,15 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
+const p = require("../profile.json");
 module.exports.run = async (bot,message,args) => {
-    message.channel.send('pong!');
-    let enbed = new Discord.RichEmbed() 
+    let uid = message.author.id;
+    let emoji = args.join(" ");
+    if ( args.length === 1 && args[ 0 ] !== 'paste' ) {
+    p[uid].copy = emoji;
+
+    } else {
+        bot.send(p[uid].copy
+    }
 };
 
 module.exports.help = {
