d2e94750967427b8dd6000fb6ca850340255fc7e
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-02 14:56:28 +0300

    Update setshop.js

--- cmds/setshop.js
+++ cmds/setshop.js
@@ -3,16 +3,18 @@ const fs = require("fs");
 const shop = require("../guildsshop.json");
 module.exports.run = async (bot,message,args) => {
   let gid = message.guild.id;
+  
+  if(shop[gid].buyrole !== "none") return bot.send("У вас уже есть роль. Удалите старую используя ``.setshop delete``");
+  let role = args.join(" ");
+  let cost = args.pop();
+  
+  shop[gid].buyrole = role;
+  shop[gid].cost = cost;
+  bot.send("Роль была добавлена в магазин вашего сервера");
   if ( args.length === 1 && args[ 0 ] === 'delete' ) {
     shop[gid].buyrole = "none"
     shop[gid].cost = "none"
   }
-  if(shop[gid].buyrole !== "none") return bot.send("У вас уже есть роль. Удалите старую используя ``.setshop delete``");
-  let role = args.join(" ");
-  let cost = args.pop();
-  shop[gid].buyrole = role;
-  shop[gid].cost = cost;
-  bot.send("Роль была добавлена в магазин вашего сервера");
 };
 
 module.exports.help = {
