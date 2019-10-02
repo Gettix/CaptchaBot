d447bb7aa19362834130c12cb4c7a1d8d9c93138
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-02 18:30:54 +0700

    Update setshop.js

--- cmds/setshop.js
+++ cmds/setshop.js
@@ -1,6 +1,10 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
+const shop = require("../guildsshop.json");
 module.exports.run = async (bot,message,args) => {
+  
+  let role = args.join(" ");
+  let cost = args.pop();
   
 };
 
