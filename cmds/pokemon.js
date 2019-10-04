99877647e9d96c124998754ee5540f3c52c16605
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-04 16:07:44 +0700

    Create pokemon.js

---
+++ cmds/pokemon.js
@@ -0,0 +1,21 @@
+    
+const Discord = module.require("discord.js");
+const fs = require("fs");
+const pokemon = require("pokemon");
+const pokegif = require("pokemon-gif");
+
+module.exports.run = async (bot,message,args) => {
+
+var poke = await pokemon.random();
+var pokegif = await pokegif(poke);
+
+let pokemed = new Discord.RichEmbed()
+.setTitle(`Вы поймали ${poke}`)
+.setImage(pokegif)
+bot.send(pokembed);
+
+};
+
+module.exports.help = {
+    name: "ping"
+};
