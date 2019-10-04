d22e415d6e5c3bd6d1e5dd06af28cd5a7c8f964c
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-04 16:29:43 +0700

    Update pokemon.js

--- cmds/pokemon.js
+++ cmds/pokemon.js
@@ -3,11 +3,15 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 const pokemon = require("pokemon");
 const pokemonGif = require("pokemon-gif");
+const inv = require("../pokemon.json");
 
 module.exports.run = async (bot,message,args) => {
 
+let uid = message.author.id;
 var poke = await pokemon.random();
 var pokegif = pokemonGif(poke);
+inv[uid].names += "," + poke;
+inv[uid].num += 1
 
 let pokemed = new Discord.RichEmbed()
 .setTitle(`Вы поймали ${poke}`)
