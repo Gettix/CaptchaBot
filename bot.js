b53957ea819a667aa41d98e3a9abfb3f4c5645ba
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-04 16:33:37 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ bot.commands = new Discord.Collection();
 const fs = require('fs');
 const token = process.env.BOT_TOKEN;
 let config = require('./botconfig.json');
-const pokemon = reqquire('./pokemon.json');
+const pokemon = require('./pokemon.json');
 
 let g = require('./game.json');
 let prefix = config.prefix;
