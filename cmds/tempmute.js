1fb51ebb1693d1756d183916057686a67b731cf1
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-05 12:05:05 +0700

    Update tempmute.js

--- cmds/tempmute.js
+++ cmds/tempmute.js
@@ -32,12 +32,12 @@ module.exports.run = async (bot, message, args) => {
   if(!mutetime) return message.reply("You didn't specify a time!");
 
   await(tomute.addRole(muterole.id));
-  message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+  message.reply(`<@${tomute.id}> has been muted for ${fs(fs(mutetime))}`);
 
   setTimeout(function(){
     tomute.removeRole(muterole.id);
     message.channel.send(`<@${tomute.id}> has been unmuted!`);
-  }, ms(mutetime));
+  }, fs(mutetime));
 
 
 //end of module
