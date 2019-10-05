b41f1369f286c9eb8ee6df265b220b3d46ccfca6
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-05 15:11:21 +0300

    Create guildoptions.js

---
+++ cmds/guildoptions.js
@@ -0,0 +1,14 @@
+const options = require("../guildsshop.json");
+const Discord = module.require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+
+let gid = message.guild.id;
+
+
+
+}
+
+module.exports.help = {
+    name: "guild"
+}
