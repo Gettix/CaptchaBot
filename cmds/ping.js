5bf8c00116aac1194b6c6853dbb11244c27df168
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-05 08:38:27 +0300

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     message.channel.send('pong!');
     let enbed = new Discord.RichEmbed() 
+    .setTitle("Lol")
+    bot.send(enbed);
+    enbed.edit(
+        .setTitle("Я лох"));
 };
 
 module.exports.help = {
