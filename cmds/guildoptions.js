91d085e637f1ee64c08c94962e2c782201c1efc4
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-05 15:15:25 +0300

    Update guildoptions.js

--- cmds/guildoptions.js
+++ cmds/guildoptions.js
@@ -3,9 +3,12 @@ const Discord = module.require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
 
+let inc = args.pop();
 let gid = message.guild.id;
 
-
+if ( args.length === 1 && args[ 0 ] === 'logs' ) {
+    options[gid]. = 
+}
 
 }
 
