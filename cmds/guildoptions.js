ef827c4fad5d91b3a8ca74e59796a86cfe3acd84
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-07 13:49:44 +0300

    Update guildoptions.js

--- cmds/guildoptions.js
+++ cmds/guildoptions.js
@@ -3,7 +3,7 @@ const Discord = module.require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
 
-let inc = args[1];
+let inc = args.pop();
 
 
 let gid = message.guild.id;
@@ -11,14 +11,14 @@ let gid = message.guild.id;
 if ( args.length === 1 && args[ 0 ] === 'logs' ) {
     if(!inc) return bot.send("Укажите канал действий");
         
-    options[gid].actionch == inc;
+    options[gid].actionch = inc;
 bot.send(`Успешно! Канал действий сменён на ${inc}`);
 };
     if ( args.length === 1 && args[ 0 ] === 'memberrole') {
         if(!inc) return bot.send("Укажите роль для новеньких");
 
  
-    options[gid].roleadd == inc;
+    options[gid].roleadd = inc;
         bot.send(`Успешно!Роль добавлена`);
 };
 }
