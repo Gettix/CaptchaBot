d1f8c0683720f1d5cd361ee4afea90fc3b307a2c
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-07 15:10:24 +0300

    Update find.js

--- cmds/find.js
+++ cmds/find.js
@@ -1,18 +1,21 @@
 const Discord = require("discord.js");
+const options = require("../guildsshop.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    let rreason = args.join(" ").slice(22);
-
-    let reportEmbed = new Discord.RichEmbed()
-    .setDescription("Find")
-    .setColor("#15f153")
-    .addField("User", `${message.author}`)
-    .addField("Find", rreason)
-    .addField("Time", message.createdAt);
-
-    message.delete().catch(O_o=>{});
-    message.channel.send(reportEmbed);
+    let gid = message.guild.id;
+    let one = options[gid].actionch;
+    let two = options[gid].addrole;
+    let tree = options[gid].temprole;
+    let four = options[gid];
+    
+    let embed = new Discord.RichEmbed()
+    .setTitle(`Опции сервера. [ID: ${four}`
+    .setColor(RANDOM)
+    .addField("Канал действий", one, true)
+    .addField("Роль прибывшим", two, true)
+    .addField("Роль мут", tree, true)
+    bot.send(embed);
 
 }
  
