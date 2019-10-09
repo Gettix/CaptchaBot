aa5d0afcf75cda69254ae639d76764597cfa8fef
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-09 14:54:15 +0700

    Update captchagen.js

--- cmds/captchagen.js
+++ cmds/captchagen.js
@@ -4,7 +4,8 @@ const profile = require("../profile.json");
 
 module.exports.run = async (bot,message,args) => {
    let lol = message.author.id;
-  
+   let gol = args.pop();
+  if ( args.length === 1 && args[ 0 ] === 'start' ) {
  // optional object arg with keys: height, width, text, font
 var captcha = captchagen.create();
  
@@ -22,6 +23,14 @@ captcha.buffer(type); // outputs an image stream. type can be either png or jpeg
   profile[lol].code = captcha.text();
  
  bot.send(captcha);
+  }
+   if ( args.length === 1 && args[ 0 ] === 'enter' ) {
+      if(profile[lol].code === gol) {
+         bot.send("Капча пройдена! Контент разблокирован");
+         profile[lol].code == 0;
+      } else {
+      profile[lol].code == 0;
+      bot.send("Вы неправильно ввели капчу!");
 }
 
 module.exports.help = {
