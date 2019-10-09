0a941cceb4ac9305f297da3930c11491fa04cc7f
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-09 15:52:33 +0700

    Update captchagen.js

--- cmds/captchagen.js
+++ cmds/captchagen.js
@@ -1,10 +1,12 @@
 const Discord = module.require("discord.js");
 const profile = require("../profile.json");
 var generator = require('generate-password');
+const g = require('../guildsshop.json');
 
 module.exports.run = async (bot,message,args) => {
    let lol = message.author.id;
    let gol = args.pop();
+   let gid = message.guild.id;
   if ( args.length === 1 && args[ 0 ] === 'start' ) {
  // optional object arg with keys: height, width, text, font
 var password = generator.generate({
@@ -23,6 +25,8 @@ var password = generator.generate({
    if ( args.length === 1 && args[ 0 ] === 'enter' ) {
       if(profile[lol].code === gol) {
          bot.send("Капча пройдена! Контент разблокирован");
+         let roless = g[gid].buyrole;
+	message.member.addRole(roless)
          profile[lol].code == 0;
       } else {
       profile[lol].code == 0;
