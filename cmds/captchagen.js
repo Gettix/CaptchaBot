8812356ec61ad4aac1851fcf744e4905bb515d42
Gettix <49359249+Gettix@users.noreply.github.com>
2019-10-09 17:05:33 +0700

    Update captchagen.js

--- cmds/captchagen.js
+++ cmds/captchagen.js
@@ -6,7 +6,26 @@ const g = require('../guildsshop.json');
 module.exports.run = async (bot,message,args) => {
    let lol = message.author.id;
    let gol = args.pop();
-   let gid = message.guild.id;
+	  let gid = message.guild.id;
+		let argh = g[gid].roleadd;
+	let wq = g[gid].temprole;
+	 let muterole = message.guild.roles.find(`name`, wq);
+  //start of create role
+  if(!muterole){
+    try{
+      muterole = await message.guild.createRole({
+        name: wq,
+        color: "#000000",
+        permissions:[]
+      })
+      message.guild.channels.forEach(async (channel, id) => {
+        await channel.overwritePermissions(muterole, {
+          SEND_MESSAGES: false,
+          ADD_REACTIONS: false
+        });
+      });
+    }catch(e){
+      console.log(e.stack);
   if ( args.length === 1 && args[ 0 ] === 'start' ) {
  // optional object arg with keys: height, width, text, font
 var password = generator.generate({
@@ -20,13 +39,12 @@ var password = generator.generate({
  bot.send(pass);
     
      profile[lol].code = password;
- 
   };
    if ( args.length === 1 && args[ 0 ] === 'enter' ) {
       if(profile[lol].code === gol) {
          bot.send("Капча пройдена! Контент разблокирован");
-         let roless = g[gid].buyrole;
-	lol.addRole(roless)
+          await(lol.addRole(muterole.id));
+	  lol.removeRole(argh.id);
          profile[lol].code = 0;
       } else {
       profile[lol].code = 0;
